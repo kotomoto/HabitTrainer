77c493e0e8445f96131ef3e23acb0e1043678b76
koto <jkotor@gmail.com>
2017-10-31 15:28:32 +0100

    Added first cardview example

--- app/src/main/java/com/koto/habittrainer/MainActivity.kt
+++ app/src/main/java/com/koto/habittrainer/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.koto.habittrainer
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        iv_icon.setImageResource(R.drawable.water)
+        tv_title.text = getString(R.string.drink_water)
+        tv_description.text = getString(R.string.drink_water_desc)
     }
 }
