fee1ce09d3e465dd37ab119b0496567495ec6c74
koto <jkotor@gmail.com>
2017-11-02 13:11:47 +0100

    Top level handling of storing new habits

--- app/src/main/java/com/koto/habittrainer/CreateHabitActivity.kt
+++ app/src/main/java/com/koto/habittrainer/CreateHabitActivity.kt
@@ -9,6 +9,7 @@ import android.os.Bundle
 import android.provider.MediaStore
 import android.util.Log
 import android.view.View
+import android.widget.EditText
 import kotlinx.android.synthetic.main.activity_create_habit.*
 import java.io.IOException
 
@@ -17,6 +18,8 @@ class CreateHabitActivity : AppCompatActivity() {
     private val TAG = CreateHabitActivity::class.java.simpleName
 
     private val CHOOSE_IMAGE_REQUEST = 1
+
+    private var imageBitmap: Bitmap? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,6 +37,25 @@ class CreateHabitActivity : AppCompatActivity() {
         Log.d(TAG, "Intent to choose image sent...")
     }
 
+    fun storeHabit(v: View) {
+        if (et_title.isBlank() || et_description.isBlank()) {
+            Log.d(TAG, "No habit stored: title or description missing.")
+            displayErrorMessage("Your habit needs an engaging title and description")
+            return
+        } else if (imageBitmap == null) {
+            Log.d(TAG, "No habit stored: image missing.")
+            displayErrorMessage("Add a motivating picture to your habit")
+            return
+        }
+
+        tv_error.visibility = View.INVISIBLE
+    }
+
+    private fun displayErrorMessage(message: String) {
+        tv_error.text = message
+        tv_error.visibility = View.VISIBLE
+    }
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
@@ -44,6 +66,7 @@ class CreateHabitActivity : AppCompatActivity() {
             val bitmap = tryReadBitmap(data.data)
 
             bitmap?.let {
+                this.imageBitmap = bitmap
                 iv_image.setImageBitmap(bitmap)
                 Log.d(TAG, "Read image bitmap and updated image view")
             }
@@ -59,3 +82,5 @@ class CreateHabitActivity : AppCompatActivity() {
         }
     }
 }
+
+private fun EditText.isBlank(): Boolean = this.text.toString().isBlank()
