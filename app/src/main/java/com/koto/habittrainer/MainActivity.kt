494171690e3fa74e234aa1ce9553dba0beed740f
koto <jkotor@gmail.com>
2017-11-02 07:59:03 +0100

    Added CreateHabitActivity with UI and some behaviour

--- app/src/main/java/com/koto/habittrainer/MainActivity.kt
+++ app/src/main/java/com/koto/habittrainer/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.koto.habittrainer
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
+import android.view.Menu
+import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_main.*
-
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +18,25 @@ class MainActivity : AppCompatActivity() {
         rv.layoutManager = LinearLayoutManager(this)
         rv.adapter = HabitsAdapter(getSampleHabits())
     }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.add_habit) {
+            switchTo(CreateHabitActivity::class.java)
+        }
+
+        return true
+    }
+
+    private fun switchTo(c: Class<*>) {
+        val intent = Intent(this, c)
+        startActivity(intent)
+    }
 }
+
 
