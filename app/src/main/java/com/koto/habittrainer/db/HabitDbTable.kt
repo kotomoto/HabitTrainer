96abd14f76270dcc5d218225335099cbffd39f87
koto <jkotor@gmail.com>
2017-11-04 15:32:24 +0100

    Code polishing

--- app/src/main/java/com/koto/habittrainer/db/HabitDbTable.kt
+++ app/src/main/java/com/koto/habittrainer/db/HabitDbTable.kt
@@ -2,11 +2,17 @@ package com.koto.habittrainer.db
 
 import android.content.ContentValues
 import android.content.Context
+import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.util.Log
 import com.koto.habittrainer.Habit
+import com.koto.habittrainer.db.HabitEntry.DESC_COL
+import com.koto.habittrainer.db.HabitEntry.IMAGE_COL
+import com.koto.habittrainer.db.HabitEntry.TABLE_NAME
+import com.koto.habittrainer.db.HabitEntry.TITLE_COL
+import com.koto.habittrainer.db.HabitEntry._ID
 import java.io.ByteArrayOutputStream
 
 class HabitDbTable(context: Context) {
@@ -20,9 +26,9 @@ class HabitDbTable(context: Context) {
 
         val values = ContentValues()
         with(values) {
-            put(HabitEntry.TITLE_COL, habit.title)
-            put(HabitEntry.DESC_COL, habit.description)
-            put(HabitEntry.IMAGE_COL, toByteArray(habit.image))
+            put(TITLE_COL, habit.title)
+            put(DESC_COL, habit.description)
+            put(IMAGE_COL, toByteArray(habit.image))
         }
 
         val id = db.transaction { insert(HabitEntry.TABLE_NAME, null, values) }
@@ -33,31 +39,36 @@ class HabitDbTable(context: Context) {
     }
 
     fun readAllHabits(): List<Habit> {
-        val columns = arrayOf(HabitEntry._ID, HabitEntry.TITLE_COL, HabitEntry.DESC_COL, HabitEntry.IMAGE_COL)
+        val columns = arrayOf(_ID, TITLE_COL, DESC_COL, IMAGE_COL)
         val db = dbHelper.readableDatabase
-        val order = "${HabitEntry._ID} ASC"
-        val cursor = db.query(HabitEntry.TABLE_NAME, columns, null, null, null, null, order)
+        val order = "$_ID ASC"
+        val cursor = db.doQuery(TABLE_NAME, columns, orderBy = order)
 
+        return parseHabitsFrom(cursor)
+    }
+
+    private fun parseHabitsFrom(cursor: Cursor): MutableList<Habit> {
         val habits = mutableListOf<Habit>()
-        while(cursor.moveToNext()) {
-            val title = cursor.getString(cursor.getColumnIndex(HabitEntry.TITLE_COL))
-            val description = cursor.getString(cursor.getColumnIndex(HabitEntry.DESC_COL))
-            val byteArray = cursor.getBlob(cursor.getColumnIndex(HabitEntry.IMAGE_COL))
-            val bitmap = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.size)
 
-            habits.add(Habit(title, description, bitmap))
+        with(cursor) {
+            while (moveToNext()) {
+                val title = getString(TITLE_COL)
+                val description = getString(DESC_COL)
+                val bitmap = getBitmap(IMAGE_COL)
+                habits.add(Habit(title, description, bitmap))
+            }
+
+            close()
         }
-
-        cursor.close()
 
         return habits
     }
+}
 
-    private fun toByteArray(bitmap: Bitmap): ByteArray {
-        val stream = ByteArrayOutputStream()
-        bitmap.compress(Bitmap.CompressFormat.PNG, 0, stream)
-        return stream.toByteArray()
-    }
+private fun toByteArray(bitmap: Bitmap): ByteArray {
+    val stream = ByteArrayOutputStream()
+    bitmap.compress(Bitmap.CompressFormat.PNG, 0, stream)
+    return stream.toByteArray()
 }
 
 private inline fun <T> SQLiteDatabase.transaction(function: SQLiteDatabase.() -> T): T {
@@ -73,4 +84,16 @@ private inline fun <T> SQLiteDatabase.transaction(function: SQLiteDatabase.() ->
     close()
 
     return result
+}
+
+private fun SQLiteDatabase.doQuery(table: String, columns: Array<String>, selection: String? = null, selectionArgs: Array<String>? = null,
+                                   groupBy: String? = null, having: String? = null, orderBy: String? = null): Cursor {
+    return query(table, columns, selection, selectionArgs, groupBy, having, orderBy)
+}
+
+private fun Cursor.getString(columnName: String) = getString(getColumnIndex(columnName))
+
+private fun Cursor.getBitmap(columnName: String): Bitmap {
+    val byteArray = getBlob(getColumnIndex(columnName))
+    return BitmapFactory.decodeByteArray(byteArray, 0, byteArray.size)
 }
